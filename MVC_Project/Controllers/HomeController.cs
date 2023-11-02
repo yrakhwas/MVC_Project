86939b82580567d9561bb70700366b5da81ec4a0
yrakhwas <yrakhwas@gmail.com>
2023-11-02 22:26:51 +0200

    Add Product Page

--- MVC_Project/Controllers/HomeController.cs
+++ MVC_Project/Controllers/HomeController.cs
@@ -6,18 +6,20 @@ namespace MVC_Project.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            
         }
 
         public IActionResult Index()
         {
             return View();
         }
-
+        public IActionResult About()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ MVC_Project/Controllers/Products.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Project.Controllers
+{
+    public class Products : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
